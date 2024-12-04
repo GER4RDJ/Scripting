0ce513638b36bb7826f32021c71fcbd0220b7c17
GER4RDJ <jurcagerard@gmail.com>
2024-12-04 18:42:37 +0100

    - Third Commit (Destroy Game Object)

---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(game0bject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
